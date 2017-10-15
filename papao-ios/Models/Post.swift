3c78366b37fd69230753979f807bba593fb4b39c
Seungwon Kim <closer527@gmail.com>
2017-10-15 23:59:47 +0900

    add model for post

---
+++ papao-ios/Models/Post.swift
@@ -0,0 +1,29 @@
+//
+//  Post.swift
+//  papao-ios
+//
+//  Created by 1002719 on 2017. 10. 14..
+//  Copyright © 2017년 papaolabs. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    var feature: String?
+    var gender: String?
+    var happenDate: String?
+    var happenPlace: String?
+    var id : Int
+    var imageUrl: String?
+    var introduction: String?
+    var kindCode: String?
+    var kindName: String?
+    var kindUpCode: String?
+    var neuter: String?
+    var state: String?
+    var type: String?
+    var userAddress: String?
+    var userContracts: String?
+    var userName: String?
+    var weight: String?
+}
