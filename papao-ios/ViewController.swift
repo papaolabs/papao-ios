02f7050c562d14dfe499031b4d2d3fa178a91b75
Seungwon Kim <closer527@gmail.com>
2017-10-16 23:52:55 +0900

    #3 post 데이터 출력을 위한 기본 UI 설정
    - 기본적인 테이블뷰 배치
    - 샘플 모델데이터 추가 표시 확인

--- papao-ios/ViewController.swift
+++ papao-ios/ViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var tableView: UITableView!
+    var posts: [Post] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let postDict: [String: String] = [
+            "id": "1",
+            "type": "01",
+            "imageUrl": "http://www.animal.go.kr/files/shelter/2017/08/201710111610187.jpg",
+            "kindUpCode": "417000",
+            "kindCode": "50",
+            "kindName": "프렌치 불독",
+            "happenDate": "20171001",
+            "happenPlace": "전라남도 나주시",
+            "userId": "01",
+            "userName": "ㅇㅇㅇ애견",
+            "userAddress": "서울시 금천구",
+            "userContracts": "010-1234-5678",
+            "weight": "4.0",
+            "gender": "F",
+            "state": "보호중",
+            "neuter": "U",
+            "feature": "프렌치불독",
+            "introduction": ""
+        ]
+        let post = Post(postDict)
+        posts.append(post)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +45,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - TableView DataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "postCellIdentifier",
+                                                                  for: indexPath)
+        let row = indexPath.row;
+        let post = posts[row]
+        
+        cell.textLabel?.text = post.kindName
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
 
+    }
+    
+    // MARK: - TableView Delegate
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 58
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
 }
 
