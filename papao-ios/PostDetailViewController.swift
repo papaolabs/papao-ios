5ec89d9fde1dbf421b69ca8f01dcc0229256e6e1
Seungwon Kim <closer527@gmail.com>
2017-10-17 16:34:32 +0900

    #3 post 상세 뷰 컨트롤러 추가 및 이미지 표시를 위한 기본 구현

---
+++ papao-ios/PostDetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  PostDetailViewController.swift
+//  papao-ios
+//
+//  Created by 1002719 on 2017. 10. 17..
+//  Copyright © 2017년 papaolabs. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class PostDetailViewController: UIViewController, UIScrollViewDelegate {
+    let post: Post? = nil
+    
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var imageViewWidth: NSLayoutConstraint!
+    @IBOutlet var imageViewHeight: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func setImageToCrop(image:UIImage){
+        imageView.image = image
+        imageViewWidth.constant = image.size.width
+        imageViewHeight.constant = image.size.height
+        let scaleHeight = scrollView.frame.size.width/image.size.width
+        let scaleWidth = scrollView.frame.size.height/image.size.height
+        let maxScale = max(scaleWidth, scaleHeight)
+        scrollView.minimumZoomScale = maxScale
+        scrollView.zoomScale = maxScale
+    }
+    
+    func crop() {
+        let scale:CGFloat = 1/scrollView.zoomScale
+        let x:CGFloat = scrollView.contentOffset.x * scale
+        let y:CGFloat = scrollView.contentOffset.y * scale
+        let width:CGFloat = scrollView.frame.size.width * scale
+        let height:CGFloat = scrollView.frame.size.height * scale
+        let croppedCGImage = imageView.image?.cgImage?.cropping(to: CGRect(x: x, y: y, width: width, height: height))
+        let croppedImage = UIImage(cgImage: croppedCGImage!)
+        setImageToCrop(image: croppedImage)
+    }
+}
