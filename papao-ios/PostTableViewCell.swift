083139cabfb8af688633417ab7234be2d1fc8008
Seungwon Kim <closer527@gmail.com>
2017-10-17 15:29:32 +0900

    #3 post 데이터 출력을 위한 커스텀셀 클래스 추가 및 바인딩

---
+++ papao-ios/PostTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PostTableViewCell.swift
+//  papao-ios
+//
+//  Created by 1002719 on 2017. 10. 17..
+//  Copyright © 2017년 papaolabs. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    @IBOutlet var postImageView: UIImageView!
+    @IBOutlet var kindLabel: UILabel!
+    @IBOutlet var happenDateLabel: UILabel!
+    @IBOutlet var happenPlaceLabel: UILabel!
+    @IBOutlet var favoriteButton: UIButton!
+}
