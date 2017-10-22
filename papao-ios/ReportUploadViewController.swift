7e1f0e828e74bf6d65664737f864e9065541de53
Seungwon Kim <closer527@gmail.com>
2017-10-22 15:13:30 +0900

    #7 포토 라이브러리 상의 이미지 가져오기 로직 추

--- papao-ios/ReportUploadViewController.swift
+++ papao-ios/ReportUploadViewController.swift
@@ -8,9 +8,34 @@
 
 import UIKit
 
-class ReportUploadViewController: UIViewController {
+class ReportUploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet var uploadButton: UIButton!
+    @IBOutlet var uploadedImageView: UIImageView!
     
+    let picker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        picker.delegate = self
+    }
+
+    // MARK: - IBActions
     @IBAction func uploadButtonPressed(button: UIButton) {
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        present(picker, animated: true, completion: nil)
+    }
+    
+    // MARK: - ImagePickerController Delegates
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        uploadedImageView.contentMode = .scaleAspectFit
+        uploadedImageView.image = chosenImage
+        dismiss(animated:true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 }
