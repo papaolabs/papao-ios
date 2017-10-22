3d99842bb050a9b28725cb3b0cf5b316063e6592
Seungwon Kim <closer527@gmail.com>
2017-10-22 14:54:10 +0900

    #7 제보 등록을 위한 기본 뷰컨트롤러 플로우 구성

---
+++ papao-ios/ReportUploadViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ReportUploadViewController.swift
+//  papao-ios
+//
+//  Created by 1002719 on 2017. 10. 22..
+//  Copyright © 2017년 papaolabs. All rights reserved.
+//
+
+import UIKit
+
+class ReportUploadViewController: UIViewController {
+    @IBOutlet var uploadButton: UIButton!
+    
+    @IBAction func uploadButtonPressed(button: UIButton) {
+    }
+}
