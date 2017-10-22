8a6f779ba4202f1b341c022f2cd0c78625a78b5a
Seungwon Kim <closer527@gmail.com>
2017-10-22 15:53:15 +0900

    #7 이미지 내 촬영시간, gps 정보 가져오기
    - 예외처리 필요

--- papao-ios/ReportUploadViewController.swift
+++ papao-ios/ReportUploadViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import Photos
+import PhotosUI
 
 class ReportUploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet var uploadButton: UIButton!
+
     @IBOutlet var uploadedImageView: UIImageView!
-    
+    @IBOutlet weak var dateTakenLabel: UILabel!
+    @IBOutlet weak var locationTakenLabel: UILabel!
+
     let picker = UIImagePickerController()
     
     override func viewDidLoad() {
@@ -32,6 +37,38 @@ class ReportUploadViewController: UIViewController, UIImagePickerControllerDeleg
         let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
         uploadedImageView.contentMode = .scaleAspectFit
         uploadedImageView.image = chosenImage
+        
+        let assetURL = info[UIImagePickerControllerReferenceURL] as! NSURL
+        let asset = PHAsset.fetchAssets(withALAssetURLs: [assetURL as URL], options: nil)
+        guard let result = asset.firstObject else {
+            return
+        }
+        
+        let imageManager = PHImageManager.default()
+        imageManager.requestImageData(for: result , options: nil, resultHandler:{
+            (data, responseString, imageOriet, info) -> Void in
+            let imageData: NSData = data! as NSData
+            if let imageSource = CGImageSourceCreateWithData(imageData, nil) {
+                let imageProperties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil)! as NSDictionary
+                print("imageProperties: ", imageProperties)
+                if let exif = imageProperties["{Exif}"] as? [String: Any] {
+                    if let date = exif["DateTimeOriginal"] {
+                        self.dateTakenLabel.text = "날짜시간: \(date as! String)"
+                    }
+                }
+                if let gps = imageProperties["{GPS}"] as? [String: Any] {
+                    if let latitude = gps["Latitude"] {
+                        self.locationTakenLabel.text = String(describing: "위치: \(latitude as! Float)")
+                    }
+                    if let longitude = gps["Longitude"] {
+                        self.locationTakenLabel.text = "\(String(describing: self.locationTakenLabel.text)), \(longitude)"
+                    }
+                }
+                
+            }
+            
+        })
+
         dismiss(animated:true, completion: nil)
     }
 
