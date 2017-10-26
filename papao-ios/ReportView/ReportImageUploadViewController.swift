31807346afb130a5b13f522c8d24aa3059584823
Seungwon Kim <closer527@gmail.com>
2017-10-26 22:17:49 +0900

    #7 등록 된 복수의 사진 표시를 위한 뷰 세팅

--- papao-ios/ReportView/ReportImageUploadViewController.swift
+++ papao-ios/ReportView/ReportImageUploadViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class ReportImageUploadViewController: UIViewController {
+class ReportImageUploadViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet var progressToolBar: UIToolbar!
     @IBOutlet weak var imageScrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var nextButton: UIButton!
     
+    // for getting a photo
+    let picker = UIImagePickerController()
+
     var uploadImages: [UIImage] = []
     
     override func viewDidLoad() {
@@ -25,5 +28,27 @@ class ReportImageUploadViewController: UIViewController {
             yPosition += heightOfNavigation
         }
         progressToolBar.frame = CGRect(x:0, y:yPosition, width:self.view.bounds.size.width, height:54)
+        
+        // set content size of scrollView
+        imageScrollView.contentSize = CGSize(width: imageScrollView.bounds.size.width * 3, height: imageScrollView.bounds.size.height)
+        imageScrollView.backgroundColor = UIColor(named: "warmPink")
+        imageScrollView.showsVerticalScrollIndicator = false
+        imageScrollView.showsHorizontalScrollIndicator = false
+        imageScrollView.alwaysBounceVertical = false
+        imageScrollView.alwaysBounceHorizontal = true
+        imageScrollView.isPagingEnabled = true
+        imageScrollView.delegate = self
+    }
+    
+    // MARK: - IBActions
+    @IBAction func pageChangeValue(sender: UIPageControl) {
+        let xPoint = sender.currentPage * Int(imageScrollView.bounds.size.width)
+        imageScrollView.setContentOffset(CGPoint.init(x: xPoint, y: 0), animated: true)
+    }
+    
+    // MARK: - UIScrollView Delegate
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageWidth = scrollView.frame.size.width
+        pageControl.currentPage = Int(floor((scrollView.contentOffset.x - pageWidth / 3) / pageWidth) + 1)
     }
 }
