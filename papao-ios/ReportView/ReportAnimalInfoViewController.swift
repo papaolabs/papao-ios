c047d0763b851923a78cbda8971e0353b3bbeab3
Seungwon Kim <closer527@gmail.com>
2017-10-27 23:39:19 +0900

    #7 동물 정보 입력 뷰 UI 레이아웃 작

---
+++ papao-ios/ReportView/ReportAnimalInfoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ReportAnimalInfoViewController.swift
+//  papao-ios
+//
+//  Created by closer27 on 2017. 10. 27..
+//  Copyright © 2017년 papaolabs. All rights reserved.
+//
+
+import UIKit
+
+class ReportAnimalInfoViewController: UIViewController {
+    
+}
