2a934212a892a6959c379bb7129740888ca240a8
Seungwon Kim <closer527@gmail.com>
2017-10-29 21:19:26 +0900

    #7 축종, 품종 데이터 표시를 위한 로직 구현

--- papao-ios/Models/Species.swift
+++ papao-ios/Models/Species.swift
@@ -1,5 +1,5 @@
 //
-//  Breed.swift
+//  Species.swift
 //  papao-ios
 //
 //  Created by closer27 on 2017. 10. 29..
@@ -8,13 +8,11 @@
 
 import Foundation
 
-struct Breed {
-    var species: Species
+struct Species {
     var code: Int
     var name: String
     
     init(dict: [String: AnyObject]) {
-        self.species = Species(dict: dict["category"] as! [String : AnyObject])
         self.code = dict["code"] as! Int
         self.name = dict["name"] as! String
     }
--- papao-ios/ReportView/ReportAnimalInfoViewController.swift
+++ papao-ios/ReportView/ReportAnimalInfoViewController.swift
@@ -10,17 +10,35 @@ import UIKit
 
 class ReportAnimalInfoViewController: UIViewController {
     var breedList: [Breed]!
+    var speciesList: [Species]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // create species list
+        if let path = Bundle.main.path(forResource: "SpeciesList", ofType: "plist"), let dict = NSDictionary(contentsOfFile: path) as? [String: AnyObject] {
+            if let speciesList: [AnyObject] = dict["Species"] as? [AnyObject] {
+                self.speciesList = speciesList.map({ (dict) -> Species in
+                    return Species(dict: dict as! [String: AnyObject])
+                })
+            }
+        }
+        
+        // create breed list
         if let path = Bundle.main.path(forResource: "BreedList", ofType: "plist"), let dict = NSDictionary(contentsOfFile: path) as? [String: AnyObject] {
-            print(dict)
             if let breedList: [AnyObject] = dict["Breed"] as? [AnyObject] {
                 self.breedList = breedList.map({ (dict) -> Breed in
-                    return Breed(dict: dict as! [String : AnyObject])
+                    return Breed(dict: dict as! [String: AnyObject])
                 })
             }
         }
     }
+    
+    // MARK: - IBActions
+    @IBAction func speciesButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func breedButtonPressed(_ sender: Any) {
+    }
+    
 }
