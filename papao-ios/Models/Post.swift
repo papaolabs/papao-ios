5294bf930a0da7c84c7d305036fa89c425515a9c
Seungwon Kim <closer527@gmail.com>
2017-11-09 22:32:14 +0900

    #9 Post 데이터 규격 변경, State enum 타입 추가

--- papao-ios/Models/Post.swift
+++ papao-ios/Models/Post.swift
@@ -32,6 +32,33 @@ enum Type: String {
     }
 }
 
+enum State: String {
+    case PROCESS = "보호중"
+    case RETURN = "종료(반환)"
+    case NATURALDEATH = "종료(자연사)"
+    case EUTHANASIA = "종료(안락사)"
+    case ADOPTION = "종료(입양)"
+    
+    var color: UIColor? {
+        switch self {
+        case .PROCESS:
+            return nil
+        case .RETURN:
+            return UIColor.init(named: "dodgerBlue")
+        case .NATURALDEATH:
+            return UIColor.init(named: "pinkishGrey")
+        case .EUTHANASIA:
+            return UIColor.init(named: "borderGray")
+        case .ADOPTION:
+            return UIColor.init(named: "warmPink")
+        }
+    }
+    
+    var keyName: String {
+        get { return String(describing: self) }
+    }
+}
+
 enum Neuter: String {
     case Y = "Y"
     case N = "N"
@@ -75,61 +102,84 @@ enum Gender: String {
 }
 
 struct Post {
-    // MARK: - Required
+    var id : Int                    // Post ID
+    var desertionId: String?        // 공공데이터 ID
+    var stateType: String?          // 보호 상태
+    var postType: String?           // Post 타입
+    var genderType: String?         // 성별
+    var neuterType: String?         // 중성화 여부
+    var imageUrls: [[String: Any]] = []    // 사진 URL
+
+    var feature: String?            // 특징(메모)
+    var shelterName: String?        // 보호소 이름
+    var managerName: String?        // 유저 이름
+    var managerContact: String?     // 유저 연락처
+
     var happenDate: String          // 발견날짜 (yyyyMMdd)
     var happenPlace: String         // 발견장소 (주소 문자열)
-    var id : Int                    // Post ID
-    var imageUrls: [String] = []    // 사진 URL
-    var kindUpCode: String          // 축종코드
-    var type: String                // Post 타입
 
-    // MARK: - Optional
-    var age: String?                // 나이
-    var feature: String?            // 특징(메모)
-    var kindCode: String?           // 품종코드
+    var upKindName: String          // 축종코드
     var kindName: String?           // 품종 이름
-    var gender: String?             // 성별
-    var introduction: String?       // 소개
-    var neuter: String?             // 중성화 여부
-    var state: String?              // 보호 상태
-    var userAddress: String?        // 유저 주소
-    var userContact: String?        // 유저 연락처
-    var userName: String?           // 유저 이름
-    var weight: String?             // 몸무게 Float
+
+    var sidoName: String?           // 시도
+    var gunguName: String?          // 군구
+
+    var age: Int?                   // 출생연도
+    var weight: Float?              // 몸무게 Float
     
-    // MARK: - For UI
-    var images: [UIImage]?
+    var hitCount: Int?              // 조회수
+    var createdDate: String?        // 등록 날짜
+    var updatedDate: String?        // 수정 날짜
     
-    init() {
-        self.happenDate = ""
-        self.happenPlace = ""
-        self.id = -1
-        self.kindUpCode = ""
-        self.type = Type.ROADREPORT.description
-    }
-    
-    init(fromDict postDict: Dictionary<String, AnyObject>) {
-        self.happenDate = postDict["happenDate"] as! String
-        self.happenPlace = postDict["happenPlace"] as! String
-        self.id = Int(postDict["id"] as! Int)
-        for imageUrl in postDict["imageUrls"] as! [String] {
+    init?(json: [String: Any]) {
+        guard let upKindName = json["upKindName"] as? String,
+            let imageUrls = json["imageUrls"] as? [[String: Any]],
+            let happenDate = json["happenDate"] as? String,
+            let happenPlace = json["happenPlace"] as? String,
+            let id = json["id"] as? Int else {
+                return nil
+        }
+
+        self.upKindName = upKindName
+        for imageUrl: [String: Any] in imageUrls {
             self.imageUrls.append(imageUrl)
         }
-        self.kindUpCode = postDict["kindUpCode"] as! String
-        self.type = postDict["type"] as! String
+        self.happenDate = happenDate
+        self.happenPlace = happenPlace
+        self.id = Int(id)
         
-        self.age = postDict["age"] as? String
-        self.feature = postDict["feature"] as? String
-        self.gender = postDict["gender"] as? String
-        self.introduction = postDict["introduction"] as? String
-        self.kindCode = postDict["kindCode"] as? String
-        self.kindName = postDict["kindName"] as? String
-        self.neuter = postDict["neuter"] as? String
-        self.state = postDict["state"] as? String
-        self.userAddress = postDict["userAddress"] as? String
-        self.userContact = postDict["userContact"] as? String
-        self.userName = postDict["userName"] as? String
-        self.weight = postDict["weight"] as? String
+        self.desertionId = json["desertionId"] as? String
+        self.stateType = json["state"] as? String
+        self.postType = json["postType"] as? String
+        self.genderType = json["gender"] as? String
+        self.neuterType = json["neuter"] as? String
+        
+        self.feature = json["feature"] as? String
+        self.shelterName = json["shelterName"] as? String
+        self.managerName = json["managerName"] as? String
+        self.managerContact = json["managerContact"] as? String
+        
+        self.kindName = json["kindName"] as? String
+        
+        self.sidoName = json["sidoName"] as? String
+        self.gunguName = json["gunguName"] as? String
+        
+        
+        if let age = json["age"] as? String {
+            self.age = Int(age)
+        }
+        
+        if let weight = json["weight"] as? String {
+            self.weight = Float(weight)
+        }
+        
+        
+        if let hitCount = json["hitCount"] as? String {
+            self.hitCount = Int(hitCount)
+        }
+        
+        self.createdDate = json["weight"] as? String
+        self.updatedDate = json["weight"] as? String
     }
     
     /**
@@ -141,8 +191,6 @@ struct Post {
         if (feature != nil && feature != "") { count += 1 }
         if (kindName != nil && kindName != "") { count += 1 }
         if (happenPlace != nil && happenPlace != "") { count += 1 }
-        if (userName != nil && userName != "") { count += 1 }
-        if (userContact != nil && userContact != "") { count += 1 }
         return count
     }
 }
