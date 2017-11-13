40193705e7c4d221be6b9f3730521e792fae8b1e
Seungwon Kim <closer527@gmail.com>
2017-11-13 23:39:24 +0900

    #4 Endpoint로 readPost API 통신 로직 구현 및 적용

--- papao-ios/Utilities/HttpHelper.swift
+++ papao-ios/Utilities/HttpHelper.swift
@@ -9,6 +9,29 @@
 import Foundation
 import Alamofire
 
+enum ApiResult<Value> {
+    case Success(value: Value)
+    case Failure(error: NSError)
+    
+    init(_ f: () throws -> Value) {
+        do {
+            let value = try f()
+            self = .Success(value: value)
+        } catch let error as NSError {
+            self = .Failure(error: error)
+        }
+    }
+    
+    func unwrap() throws -> Value {
+        switch self {
+        case .Success(let value):
+            return value
+        case .Failure(let error):
+            throw error
+        }
+    }
+}
+
 enum Endpoint {
     case readPosts()
     case createPost()
@@ -75,6 +98,22 @@ enum Endpoint {
     }
 }
 
-class HttpHelper {
+final class HttpHelper {
+    private let manager: SessionManager
     
+    init(manager: SessionManager = SessionManager.default) {
+        self.manager = manager
+    }
+    
+    // MARK: - Public Methods
+    func readPost(postId: Int, completion: @escaping (ApiResult<PostDetail>) -> Void) {
+        manager.request(Endpoint.readPost(postId: "\(postId)").url).responseString { response in
+            if let dict = response.value?.dictionaryFromJSON(), let postDetail = PostDetail(json: dict) {
+                completion(ApiResult{ return postDetail })
+            } else {
+                completion(ApiResult.Failure(error: NSError(domain: "com.papaolabs.papao-ios", code: 1001, userInfo: [NSLocalizedDescriptionKey : "Invalid Data"])))
+            }
+        }
+    }
 }
+
