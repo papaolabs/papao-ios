367c356584bbaad0706c4e066facb297c1798de0
Seungwon Kim <closer527@gmail.com>
2017-11-16 00:37:29 +0900

    #18 제보 데이터 목록 조회 API 연동
    - 임시보호, 길거리 제보 두 타입 모두다 받아서 처리해야 함

--- papao-ios/ReportView/ReportTableViewController.swift
+++ papao-ios/ReportView/ReportTableViewController.swift
@@ -2,22 +2,87 @@
 //  ReportListViewController.swift
 //  papao-ios
 //
-//  Created by 1002719 on 2017. 10. 22..
+//  Created by closer27 on 2017. 10. 22..
 //  Copyright © 2017년 papaolabs. All rights reserved.
 //
 
 import UIKit
 
-class ReportTableViewController: UIViewController, UITabBarDelegate, UITableViewDataSource {
-    var report: [Any]?
+class ReportTableViewController: UIViewController {
+    @IBOutlet var tableView: UITableView!
+    var posts: [Post] = []
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let api = HttpHelper.init()
+        api.readPosts(postType: PostType.ROADREPORT, completion: { (result) in
+            // Todo: - PROTECTING, ROADREPORT 둘다 받아서 보여줘야함
+            do {
+                self.posts = try result.unwrap()
+                self.tableView.reloadData()
+            } catch {
+                print(error)
+            }
+        })
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension ReportTableViewController: UITableViewDelegate, UITableViewDataSource {
+    // MARK: - TableView DataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "reportCellIdentifier",
-                                                                  for: indexPath)
+        let cell: PostTableViewCell = tableView.dequeueReusableCell(withIdentifier: "postCellIdentifier",
+                                                                    for: indexPath) as! PostTableViewCell
+        let row = indexPath.row;
+        let post = posts[row]
+        
+        cell.setPost(post: post)
+        cell.kindLabel.text = post.kindName
+        cell.happenDateLabel.text = post.happenDate
+        cell.happenPlaceLabel.text = post.happenPlace
+        
+        if post.imageUrls.count > 0 {
+            if let url = post.imageUrls[0]["url"] as? String {
+                Alamofire.request(url).responseData { response in
+                    if let data = response.result.value {
+                        let image = UIImage(data: data)
+                        cell.postImageView.image = image
+                    }
+                }
+            }
+        }
+        
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+    }
+    
+    // MARK: - TableView Delegate
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let row = self.posts[indexPath.row]
+        guard let postDetailViewController = self.storyboard?.instantiateViewController(withIdentifier: "PostDetail") as? PostDetailViewController else {
+            return
+        }
+        
+        postDetailViewController.postId = row.id
+        self.navigationController?.pushViewController(postDetailViewController, animated: true)
     }
 }
