2af973f74fbfa2c97a34eebfe49f25aa9d9b0457
Seungwon Kim <closer527@gmail.com>
2017-11-15 22:45:00 +0900

    #18 실종 리스트 조회를 위한 뷰컨트롤러 생성 및 레이아웃 설정

---
+++ papao-ios/MissingView/MissingTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MissingTableViewController.swift
+//  papao-ios
+//
+//  Created by closer27 on 2017. 11. 15..
+//  Copyright © 2017년 papaolabs. All rights reserved.
+//
+
+import UIKit
+
+class MissingTableViewController: UIViewController {
+    
+}
