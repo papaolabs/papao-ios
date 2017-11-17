a30699ff199d912a3c2095ff30765fea0d052566
Seungwon Kim <closer527@gmail.com>
2017-11-17 21:33:26 +0900

    #21 join api 연동

--- papao-ios/LoginViewController.swift
+++ papao-ios/LoginViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AccountKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
     fileprivate var accountKit = AKFAccountKit(responseType: .accessToken)
     fileprivate var pendingLoginViewController: AKFViewController? = nil
     fileprivate var showAccountOnAppear = false
@@ -43,12 +43,9 @@ class LoginViewController: UIViewController {
     // MARK: - actions
     
     @IBAction func loginWithPhone(_ sender: AnyObject) {
-        if let viewController = accountKit.viewControllerForPhoneLogin(with: nil, state: nil) as? AKFViewController {
-            prepareLoginViewController(viewController)
-            if let viewController = viewController as? UIViewController {
-                present(viewController, animated: true, completion: nil)
-            }
-        }
+        let viewController = accountKit.viewControllerForPhoneLogin(with: nil, state: nil)
+        prepareLoginViewController(viewController)
+        present(viewController, animated: true, completion: nil)
     }
     
     // MARK: - helpers
@@ -56,19 +53,46 @@ class LoginViewController: UIViewController {
     fileprivate func prepareLoginViewController(_ loginViewController: AKFViewController) {
         loginViewController.delegate = self
     }
+    
+    
+    fileprivate func alert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .cancel) { (_) in
+        }
+        alert.addAction(okAction)
+        self.present(alert, animated: false)
+    }
 }
 
 // MARK: - AKFViewControllerDelegate extension
 
 extension LoginViewController: AKFViewControllerDelegate {
-    private func viewController(_ viewController: UIViewController!, didCompleteLoginWith accessToken: AKFAccessToken!, state: String!) {
-        // Todo: - 서버로 account token post 해줘야함.
-        DispatchQueue.main.async(execute: {
-            self.dismiss(animated: true, completion: nil)
-        })
+    func viewController(_ viewController: (UIViewController & AKFViewController)!, didCompleteLoginWith accessToken: AKFAccessToken!, state: String!) {
+        accountKit.requestAccount { [weak self] (account, error) in
+            if let error = error {
+                self?.alert(message: "사용자 데이터를 받는데 실패하였습니다. 다시 시도해주세요.")
+            } else {
+                if let phoneNumber = account?.phoneNumber?.phoneNumber {
+                    let api = HttpHelper.init()
+                    let parameter = ["phone": phoneNumber, "userId": accessToken.accountID, "userToken": accessToken.tokenString] as [String: AnyObject]
+                    api.join(parameters: parameter, completion: { (result) in
+                        do {
+                            // Todo: - 앱에 사용자 정보를 저장해야하나?
+                            let user = try result.unwrap()
+                            print(user)
+                            DispatchQueue.main.async(execute: {
+                                self?.dismiss(animated: true, completion: nil)
+                            })
+                        } catch {
+                            print(error)
+                        }
+                    })
+                }
+            }
+        }
     }
     
-    private func viewController(_ viewController: UIViewController!, didFailWithError error: Error!) {
+    func viewController(_ viewController: (UIViewController & AKFViewController)!, didFailWithError error: Error!) {
         print("\(viewController) did fail with error: \(error)")
     }
 }
