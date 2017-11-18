654108b9bfbb597098e0b606fe6134760f06167c
Seungwon Kim <closer527@gmail.com>
2017-11-18 20:23:50 +0900

    #17 region 데이터 삽입, filter 클래스 생성

---
+++ papao-ios/Models/Filter.swift
@@ -0,0 +1,30 @@
+//
+//  Filter.swift
+//  papao-ios
+//
+//  Created by closer27 on 2017. 11. 18..
+//  Copyright © 2017년 papaolabs. All rights reserved.
+//
+
+import Foundation
+
+struct Filter {
+    var species: String
+    var breed: String
+    var gender: String
+    var beginDate: String
+    var endDate: String
+    var sidoCode: String
+    var gungoCode: String
+    
+    init() {
+        species = ""
+        breed = ""
+        gender = ""
+        beginDate = ""
+        endDate = ""
+        sidoCode = ""
+        gungoCode = ""
+    }
+}
+
