382ff63bdc37cea1761d68b9e67325aea6dc5d45
Seungwon Kim <closer527@gmail.com>
2017-11-18 17:50:10 +0900

    #17 필터 뷰컨트롤러 레이아웃 구성

---
+++ papao-ios/FilterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FilterViewController.swift
+//  papao-ios
+//
+//  Created by closer27 on 2017. 11. 18..
+//  Copyright © 2017년 papaolabs. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
