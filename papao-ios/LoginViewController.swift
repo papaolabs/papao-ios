86cab5d1a2558b1f09ae61bc102abbf17a5afc1f
Seungwon Kim <closer527@gmail.com>
2017-11-25 22:35:25 +0900

    #49  AccountManager 클래스를 통해 로그인 여부 확인하도록 수정

--- papao-ios/LoginViewController.swift
+++ papao-ios/LoginViewController.swift
@@ -14,8 +14,6 @@ final class LoginViewController: UIViewController {
     
     fileprivate var accountKit = AKFAccountKit(responseType: .accessToken)
     fileprivate var pendingLoginViewController: AKFViewController? = nil
-    fileprivate var showAccountOnAppear = false
-    fileprivate let api = HttpHelper.init()
 
     // MARK: - view management
     
@@ -26,15 +24,13 @@ final class LoginViewController: UIViewController {
         loginButton.setTitleColor(.white, for: .normal)
         loginButton.setRadius(radius: 24)
         
-        showAccountOnAppear = accountKit.currentAccessToken != nil
         pendingLoginViewController = accountKit.viewControllerForLoginResume()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        if showAccountOnAppear {
-            showAccountOnAppear = false
+        if AccountManager.sharedInstance.isLoggedUserValid() {
             DispatchQueue.main.async(execute: {
                 self.dismiss(animated: true, completion: nil)
             })
@@ -82,6 +78,7 @@ extension LoginViewController: AKFViewControllerDelegate {
         AccountManager.sharedInstance.loginIfNotLogged { (isSuccess) -> (Void) in
             if isSuccess {
                 // 저장된 푸시디바이스토큰이 있으면 서버로 전송
+                // Todo: - 콜백처리 안하고 제대로 전송 됐는지 확인할 방법 찾기.
                 AccountManager.sharedInstance.setDeviceToken()
                 
                 // 로그인창 닫기
@@ -89,32 +86,13 @@ extension LoginViewController: AKFViewControllerDelegate {
                     self.dismiss(animated: true, completion: nil)
                 })
             } else {
-                print("로그인 실패")
-                // Todo: - 로그인 재시도 안내 팝업
+                self.alert(message: "로그인이 문제가 발생했습니다. 다시 시도해주세요.")
             }
         }
     }
     
     func viewController(_ viewController: (UIViewController & AKFViewController)!, didFailWithError error: Error!) {
         print("\(viewController) did fail with error: \(error)")
-    }
-    
-    fileprivate func sendDeviceToken(userId: String, callback: @escaping () -> Void) {
-        guard let storedDeviceToken = UserDefaults.standard.object(forKey: UserDefaultsKeys.deviceToken.rawValue) else {
-            return
-        }
-        
-        let parameters = ["deviceId": storedDeviceToken, "type": PushType.USER.rawValue, "userId": userId]
-
-        self.api.setPush(parameters: parameters as [String: AnyObject], completion: { (result) in
-            do {
-                let result = try result.unwrap()
-                print(result)
-                callback()
-            } catch {
-                print(error)
-                callback()
-            }
-        })
+        self.alert(message: "로그인에 문제가 발생했습니다. 다시 시도해주세요.")
     }
 }
