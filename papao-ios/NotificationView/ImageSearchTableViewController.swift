850a6da6521ddaab7966564cb20f069d31874e0f
Seungwon Kim <closer527@gmail.com>
2017-11-27 21:08:25 +0900

    #60 이미지 검색 결과 출력을 위한 테이블뷰 레이아웃 구성

---
+++ papao-ios/NotificationView/ImageSearchTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ImageSearchTableViewController.swift
+//  papao-ios
+//
+//  Created by closer27 on 2017. 11. 27..
+//  Copyright © 2017년 papaolabs. All rights reserved.
+//
+
+import UIKit
+
+class ImageSearchTableViewController: UIViewController {
+    
+}
